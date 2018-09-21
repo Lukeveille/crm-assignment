ce8704fb7815774c8f71639d560ad940a5a26792
Lukeveille <luke@lukeleveille.com>
2018-09-21 16:01:23 -0400

    Successfully linked the show_contact template to each contacts edit button

--- server.rb
+++ server.rb
@@ -11,3 +11,19 @@ get "/home" do
   @all_contacts = Contact.all
   erb :home
 end
+
+get "/add_contact" do
+  @title = "Add a Contact"
+  erb :add_contact
+end
+
+get "/about" do
+  @title = "About Me"
+  erb :about
+end
+
+get "/contacts/:id" do
+  params[:id]
+  @contact = Contact.find_by({id: params[:id].to_i})
+  erb :show_contact
+end
