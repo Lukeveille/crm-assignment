79944a83b742ebd43aae041cf43817ee1b84641d
Lukeveille <luke@lukeleveille.com>
2018-09-21 16:39:32 -0400

    Completed main website layout and linking

--- server.rb
+++ server.rb
@@ -25,5 +25,9 @@ end
 get "/contacts/:id" do
   params[:id]
   @contact = Contact.find_by({id: params[:id].to_i})
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    erb :error
+  end
 end
